a7459d2732d20d6bb81a48d5596a15245b9f1a53
mlosier <mlosier@newrelic.com>
2021-04-27 09:13:49 -0700

    chore(CustomerApp): root path always redirects to customers

--- client/src/components/CustomerApp/CustomerApp.js
+++ client/src/components/CustomerApp/CustomerApp.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "../../sass/styles.scss";
 
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 
 import CustomersView from "../CustomersView";
 import { customerService } from "../../services";
@@ -16,6 +16,9 @@ export default class CustomerApp extends React.Component {
       <div className="customer-app">
         <Router>
           <Switch>
+            <Route exact path="/">
+              <Redirect to="/customers" />
+            </Route>
             <Route path="/customers">
               <CustomersView customerService={customerService} />
             </Route>
