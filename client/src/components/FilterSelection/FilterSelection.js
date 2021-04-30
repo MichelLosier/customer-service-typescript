3ea26eefe42896495ed7682133df8ec82c908143
mlosier <mlosier@newrelic.com>
2021-04-30 13:13:59 -0700

    feat(FilterSelection): Adds company filter to customers view

---
+++ client/src/components/FilterSelection/FilterSelection.js
@@ -0,0 +1,89 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+export default class FilterSelection extends React.Component {
+  static propTypes = {
+    options: PropTypes.arrayOf(
+      PropTypes.shape({
+        name: PropTypes.string,
+        value: PropTypes.any,
+      })
+    ),
+    onChange: PropTypes.func,
+    selectedValue: PropTypes.any,
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      collapsed: true,
+    };
+
+    this.handleSelectedItemClick = this.handleSelectedItemClick.bind(this);
+    this.handleItemClick = this.handleItemClick.bind(this);
+  }
+
+  optionsList(options) {
+    return options.map((option, i) => {
+      return (
+        <div
+          className={"selection-item"}
+          value={option.value}
+          key={`${i}-${option.value}`}
+          onClick={() => this.handleItemClick(option.value)}
+        >
+          {option.name}
+        </div>
+      );
+    });
+  }
+
+  handleSelectedItemClick() {
+    this.setState((prevState) => {
+      return {
+        collapsed: !prevState.collapsed,
+      };
+    });
+  }
+
+  handleItemClick(evt) {
+    const { onChange } = this.props;
+    this.setState({ collapsed: true });
+    onChange(evt);
+  }
+
+  findSelectedOption(selectedValue, defaultValue, options) {
+    let defaultOption;
+    let selectedOption = null;
+
+    for (let i = 0; i < options.length; i++) {
+      const option = options[i];
+
+      if (option.value == defaultValue) {
+        defaultOption = option;
+      }
+
+      if (option.value == selectedValue) {
+        selectedOption = option;
+        break;
+      }
+    }
+
+    return selectedOption || defaultOption;
+  }
+
+  render() {
+    const { collapsed } = this.state;
+    const { options, selectedValue, defaultValue } = this.props;
+    const selectedOption = this.findSelectedOption(selectedValue, defaultValue, options);
+
+    return (
+      <div className="filter-selection">
+        <div className="selected-item" onClick={this.handleSelectedItemClick}>
+          {selectedOption.name}
+        </div>
+        <div className="selection-items">{!collapsed && this.optionsList(options)}</div>
+      </div>
+    );
+  }
+}
